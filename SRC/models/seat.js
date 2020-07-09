c5c821c09a86dec5d27f189b64315470ceaad07a
Jesus1994-7 <jesus1994_cg@hotmail.com>
2020-07-09 20:33:23 +0200

    Create Model Seat and add paranoid at Model Company

--- SRC/models/seat.js
+++ SRC/models/seat.js
@@ -3,7 +3,7 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Company extends Model {
+  class Seat extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -13,12 +13,13 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   };
-  Company.init({
-    name: DataTypes.STRING
+  Seat.init({
+    type: DataTypes.STRING,
+    price: DataTypes.STRING
   }, {
     paranoid : true,
     sequelize,
-    modelName: 'Company',
+    modelName: 'Seat',
   });
-  return Company;
+  return Seat;
 };
