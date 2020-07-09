c7cc21171bcf01202646be0833a1e6b97a894567
Christian <chsanleo@gmail.com>
2020-07-10 00:28:11 +0200

    Create code for App

--- SRC/app.js
+++ SRC/app.js
@@ -1,1 +1,11 @@
-/*C */
+const express = require('express');
+const app = express();
+
+const properties = require('./config/properties');
+//const dbconnect = require('./config/mongoDb');
+//dbconnect();
+
+
+app.use(express.json);
+
+app.listen(properties.server_PORT, () => console.log('Server running on port ' + properties.server_PORT));
