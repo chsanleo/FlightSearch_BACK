9c3d1fcf25dc056a655eea8ff3f6e0554b33f67c
hanschris2 <hanschris96@gmail.com>
2020-07-09 23:40:01 +0200

    Create model Insurance

---
+++ SRC/migrations/20200709213943-create-insurance.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Insurances', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      type: {
+        type: Sequelize.STRING
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      company: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Insurances');
+  }
+};
---
+++ SRC/models/insurance.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Insurance extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Insurance.init({
+    type: DataTypes.STRING,
+    name: DataTypes.STRING,
+    company: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Insurance',
+  });
+  return Insurance;
+};
