a418074e6448da778b9580da6fb6492d73ae34e0
Jesus1994-7 <jesus1994_cg@hotmail.com>
2020-07-10 13:29:50 +0200

    Relations ContactInfo

--- SRC/models/contactinfo.js
+++ SRC/models/contactinfo.js
@@ -10,6 +10,11 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
+      ContactInfo.hasOne(models.Airport); //Revisar
+      ContactInfo.hasOne(models.Company);
+      ContactInfo.hasOne(models.User);
+      ContactInfo.hasOne(models.Insurance);
+
       // define association here
     }
   };
