33f52bff2528749b01064a0c321077ba0065fc26
Jesus1994-7 <jesus1994_cg@hotmail.com>
2020-07-10 11:34:55 +0200

    Modify insurance

--- SRC/migrations/20200709213943-create-insurance.js
+++ SRC/migrations/20200709213943-create-insurance.js
@@ -17,6 +17,9 @@ module.exports = {
       company: {
         type: Sequelize.STRING
       },
+      price: {
+        type: Sequelize.INTEGER
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
