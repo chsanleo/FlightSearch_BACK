23ee6fc3a01dbeec28ddb309d6c62a1405317fc1
Jesus1994-7 <jesus1994_cg@hotmail.com>
2020-07-10 13:28:08 +0200

    Relations Airport

--- SRC/models/airport.js
+++ SRC/models/airport.js
@@ -10,6 +10,10 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
+      Airport.hasOne(models.IataCode);
+      Airport.hasOne(models.Country);
+      Airport.hasOne(models.ContactInfo);
+      Airport.belongsTo(models.Flight); //Revisar
      
       // define association here
     }
