6002da24ab0338cf637690700fc7b271bc18e5e3
Jesus1994-7 <jesus1994_cg@hotmail.com>
2020-07-10 13:33:38 +0200

    Relations PayData

--- SRC/models/paydata.js
+++ SRC/models/paydata.js
@@ -10,6 +10,8 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
+      PayData.belongsTo(models.UserPayDate);
+      
       // define association here
     }
   };
