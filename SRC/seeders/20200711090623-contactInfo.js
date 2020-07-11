949f3efc17cbc32267f58940284b2fbbab41035e
Christian <chsanleo@gmail.com>
2020-07-11 11:47:55 +0200

    Fill seed with info

--- SRC/seeders/20200711090623-contactInfo.js
+++ SRC/seeders/20200711090623-contactInfo.js
@@ -2,23 +2,19 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    return queryInterface.bulkInsert('contactInfos',
+    [
+      {address:"Palomino Street 30", telephone:"123456",email:"contact@info.com"},
+      {address:"Menganito Street 15", telephone:"789123",email:"contact@info.com"},
+      {address:"Fulanito Street 28", telephone:"456789",email:"contact@info.com"},
+      {address:"Sesame Street", telephone:"147258",email:"contact@info.com"},
+      {address:"Yellowstone Park", telephone:"369147",email:"contact@info.com"},
+      {address:"Baker Street", telephone:"258369",email:"contact@info.com"},
+      {address:"5th Avenue", telephone:"159753",email:"contact@info.com"}
+    ]);
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    return queryInterface.bulkDelete('contactInfos', null, {});
   }
 };
