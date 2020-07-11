86555cc4da48ffe8c4d9e2d98a99945b3791f19f
Christian <chsanleo@gmail.com>
2020-07-12 01:30:35 +0200

    Create method Login

--- SRC/controller/mainController.js
+++ SRC/controller/mainController.js
@@ -3,7 +3,32 @@
 const axios = require('axios');
 
 const MainController = {
+    async login(req, res) {
+        try {
+            //tema de email/username
+            const user = await User.findOne(
+                {
+                    where: { username: req.body.username }
+                }
+            );
+            if (!await bycrypt.compare(req.body.password, user.password)) {
+                throw new Error({ message: 'Wrong Credentials' });
+            }
 
+            const token = jwt.sign({ id: user.id }, properties.token_SECRETWORD, { expiresIn: properties.token_EXPIRES });
+
+            await User.updateOne({
+                token:token
+            },{
+                where: { id: user.id }
+            });
+
+            res.status(200).send({ user, token });
+        } catch (error) {
+            console.log(error);
+            res.status(500).send({ message: 'There was an error. Contact with the administrator.' });
+        }
+    },
 
 };
 
