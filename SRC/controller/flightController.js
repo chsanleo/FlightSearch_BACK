3a09ed305b6c815142dbd4f6c8a4229aca38d16d
Christian <chsanleo@gmail.com>
2020-07-11 15:40:00 +0200

    Create basic CRUD for FlightTicket

--- SRC/controller/flightController.js
+++ SRC/controller/flightController.js
@@ -1,56 +1,56 @@
 const { Flight } = require('../models');
 
 const FlightController = {
-    async getAllFlight (req,res) {
+    async getAllFlight(req, res) {
         try {
             const flights = await Flight.findAll();
             res.status(200).send(flights);
         } catch (error) {
             console.log(error);
-            res.status(500).send({ message : 'There was an error. Contact with the administrator.'});
+            res.status(500).send({ message: 'There was an error. Contact with the administrator.' });
         }
     },
-    async getFlight (req,res) {
+    async getFlight(req, res) {
         try {
             const flight = await Flight.findOne({
-                where : { id : req.params }
+                where: { id: req.params }
             });
             res.status(200).send(flight);
         } catch (error) {
             console.log(error);
-            res.status(500).send({ message : 'There was an error. Contact with the administrator.'});
+            res.status(500).send({ message: 'There was an error. Contact with the administrator.' });
         }
     },
-    async createFlight (req,res) {
+    async createFlight(req, res) {
         try {
-            const flight = Plane.create(req.body);
+            const flight = Flight.create(req.body);
             res.status(201).send(flight);
         } catch (error) {
             console.log(error);
-            res.status(500).send({ message : 'There was an error. Contact with the administrator.'});
+            res.status(500).send({ message: 'There was an error. Contact with the administrator.' });
         }
     },
-    async update(req,res) {
+    async update(req, res) {
         try {
             await Flight.update(req.body, {
                 where: { id: req.params }
             });
-            res.status(202).send({ message : 'Successfull Updated.'});
+            res.status(202).send({ message: 'Successfull Updated.' });
         } catch (error) {
             console.log(error);
-            res.status(500).send({ message : 'There was an error.'});
+            res.status(500).send({ message: 'There was an error.' });
         }
 
     },
-    async delete(req,res) {
+    async delete(req, res) {
         try {
             await Flight.destroy({
                 where: { id: req.params }
             });
-            res.status(202).send({ message : 'Successfull Deleted.'});
+            res.status(202).send({ message: 'Successfull Deleted.' });
         } catch (error) {
             console.log(error);
-            res.status(500).send({ message : 'There was an error.'});
+            res.status(500).send({ message: 'There was an error.' });
         }
     }
 };
--- SRC/controller/flightTicketController.js
+++ SRC/controller/flightTicketController.js
@@ -1,10 +1,34 @@
 const { Bill } = require('../models');
 
-const axios = require('axios');
-
 const FlightTicketController = {
 
-
+    async getAll(req, res) {
+        try {
+            const flightTickets = await Bill.findAll();
+            res.status(200).send(flightTickets);
+        } catch (error) {
+            console.log(error);
+            res.status(500).send({ message: 'There was an error. Contact with the administrator.' });
+        }
+    },
+    async get(req, res) {
+        try {
+            const flightTicket = await Bill.findOne({ '_id': req.params });
+            res.status(200).send(flightTicket);
+        } catch (error) {
+            console.log(error);
+            res.status(500).send({ message: 'There was an error. Contact with the administrator.' });
+        }
+    },
+    async createUpdate(req, res) {
+        try {
+            const flightTicket = await Bill.updateOne({ '_id': req.body._id }, req.body, { upsert: true });
+            res.status(202).send(flightTicket);
+        } catch (error) {
+            console.log(error);
+            res.status(500).send({ message: 'There was an error.' });
+        }
+    }
 };
 
 module.exports = FlightTicketController;
