6db93122390194769c8100427cd126bdcdaf506f
Christian <chsanleo@gmail.com>
2020-07-11 12:07:58 +0200

    Fill with data of flights seed

--- SRC/seeders/20200711095818-flight.js
+++ SRC/seeders/20200711095818-flight.js
@@ -2,23 +2,17 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  return queryInterface.bulkInsert('Flights',
+    [ 
+      { price:500.50, code: "A550B", takeOffDat: Date.now().AddDays(2), landingDate: Date.now().AddDays(3)},
+      { price:250.2, code: "B660D", takeOffDat: Date.now().AddDays(5), landingDate: Date.now().AddDays(7)},
+      { price:1564.0, code: "1550A", takeOffDat: Date.now().AddDays(4), landingDate: Date.now().AddDays(4)},
+      { price:7895.2, code: "YUR89", takeOffDat: Date.now().AddDays(1), landingDate: Date.now().AddDays(2)},
+      { price:1223.0, code: "AXA20", takeOffDat: Date.now().AddDays(1), landingDate: Date.now().AddDays(1)}
+    ]);
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    return queryInterface.bulkDelete('Flights', null, {});
   }
 };
