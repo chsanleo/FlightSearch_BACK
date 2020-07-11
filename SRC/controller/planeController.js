161d18d04d7beaf9f98ca7a62ef25ee3410a37ff
Jesus1994-7 <jesus1994_cg@hotmail.com>
2020-07-11 13:17:35 +0200

    Create Planecontroller

--- SRC/controller/planeController.js
+++ SRC/controller/planeController.js
@@ -1,8 +1,71 @@
 const { Plane } = require('../models');
 
 const axios = require('axios');
+const { update, delete } = require('../../../../KATAS/Back-end/EXPRESS-MYSQL/api/controladores/MovieControlador');
 
 const PlaneController = {
+
+    async getPlanes(req,res) {
+        try {
+            const planes = await Plane.findAll()
+            res.status(201).send(planes)
+        } catch (error) {
+            console.log(error)
+            res.status(500).send({ message : 'There was a problem get All planes'})
+        }
+    },
+    async getOnePlane (req,res) {
+        try {
+            const { id } = req.params;
+            const plane = await Plane.findOne({
+                where : {
+                    id : id
+                }
+            })
+            res.status(201).send(plane)
+        } catch (error) {
+            console.log(error)
+            res.status(500).send({ message : 'Error obteniendo un avión'})
+        }
+    },
+    async createPlane (req,res) {
+        try {
+            const plane = Plane.create(req.body)
+            res.status(201).send(plane)
+        } catch (error) {
+            console.log(error)
+            res.status(500).send({ message : 'Error creando el avion'})
+        }
+    },
+    async update(req,res) {
+        try {
+            const { id } = req.params;
+            Plane.update(req.body, {
+                where: {
+                    id: id
+                }
+            })
+            res.status(201).send({ message : 'Avion actualizado correctamente'})
+        } catch (error) {
+            console.log(error)
+            res.status(500).send({ message : 'Error actualizando el avión'})
+        }
+
+    },
+    async delete(req,res) {
+        try {
+            const { id } = req.params;
+            Plane.destroy({
+                where: {
+                    id: id
+                }
+            })
+            res.status(201).send({ message : 'Avión eliminado con éxito'})
+        } catch (error) {
+            console.log(error)
+            res.status(500).send({ message : 'Error eliminando el avión'})
+        }
+    }
 
 
 };
