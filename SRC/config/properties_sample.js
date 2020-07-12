d7b6ad345c232f5f546a247fc35de1b66cecd327
Christian <chsanleo@gmail.com>
2020-07-12 15:51:08 +0200

    Update Properties_sample file

--- SRC/config/properties_sample.js
+++ SRC/config/properties_sample.js
@@ -2,7 +2,10 @@
 const PROPERTIES = {
     server_PORT: 0,
     token_SECRETWORD : 'sample',
-    token_EXPIRES: '1s'
+    token_EXPIRES: '1s',
+    MONGO_USER: 'user', //use inside MongoUri
+    MONGO_PASS: 'pass', //use inside MongoUri
+    MONGO_NAMEDB: 'db' //use inside MongoUri
 };
 
 module.exports = PROPERTIES
