42af11c088504486763a21b41f8b9988a5051908
Christian <chsanleo@gmail.com>
2020-07-12 20:47:10 +0200

    Create relations in User,Insurance,Company with contactInfo

--- SRC/seeders/20200711104322-Insurance.js
+++ SRC/seeders/20200711104322-Insurance.js
@@ -4,9 +4,9 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('insurances', 
     [
-      {type : 'low', name: 'Pack1', company:'Branson Air Express', price: 20 },
-      {type : 'meddium', name: 'Pack2', company:'VASCO', price: 25 },
-      {type : 'high', name: 'Pack3', company:'Avianca', price: 30 },
+      {type : 'low', name: 'Pack1', company:'Branson Air Express', price: 20, ContactInfoId:24 },
+      {type : 'meddium', name: 'Pack2', company:'VASCO', price: 25, ContactInfoId:25 },
+      {type : 'high', name: 'Pack3', company:'Avianca', price: 30, ContactInfoId:26 },
     ])
   },
 
