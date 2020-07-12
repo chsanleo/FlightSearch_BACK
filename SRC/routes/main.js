c6706c5be9d86885a4033d36313b7813d69deedd
Jesus1994-7 <jesus1994_cg@hotmail.com>
2020-07-12 17:41:06 +0200

    Create registerMethod and routes of Main

--- SRC/routes/main.js
+++ SRC/routes/main.js
@@ -1,11 +1,8 @@
 const router = require('express').Router();
 const MainController = require ('../controller/mainController')
 
-//router.get('/allmain', MainController)
-//router.get('/main/:id', MainController)
-//router.post('/create', MainController)
-//router.put('/modify/:id', MainController)
-//router.delete('/delete/:id', MainController)
-router.post('/register', MainController.register)
+router.post('/login', MainController.login);
+router.get('/main/:id', MainController.getAllQuestions);
+router.post('/register', MainController.register);
 
 module.exports = router;
