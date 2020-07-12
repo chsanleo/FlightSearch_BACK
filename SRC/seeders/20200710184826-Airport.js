98d1d9c5af79371d86aabfea7b0dda7c9525a429
Jesus1994-7 <jesus1994_cg@hotmail.com>
2020-07-12 13:29:22 +0200

    Add ContactInfoId at AirportSeed

--- SRC/seeders/20200710184826-Airport.js
+++ SRC/seeders/20200710184826-Airport.js
@@ -5,66 +5,82 @@ module.exports = {
       return queryInterface.bulkInsert('Airport', [{
         name: 'Aeroparque Jorge Newbery',
         smooking: true,
+        ContactInfoId: 1
       },
       {
         name: 'Aeropuerto de Málaga-Costa del Sol',
         smooking: true,
+        ContactInfoId: 2
       },
       {
         name: 'Aeropuerto de Ámsterdam-Schiphol',
         smooking: false,
+        ContactInfoId: 3
       },
       {
         name: 'Aeropuerto Internacional Reina Beatrix',
         smooking: true,
+        ContactInfoId: 4
       },
       {
         name: 'Aeropuerto de Barcelona-El Prat',
         smooking: false,
+        ContactInfoId: 5
       },
       {
         name: 'Aeropuerto de Bamiyán',
         smooking: true,
+        ContactInfoId: 6
       },
       {
         name: 'Aeropuerto Internacional El Dorado',
         smooking: false,
+        ContactInfoId: 7
       },
       {
         name: 'Base Aérea de Beja',
         smooking: false,
+        ContactInfoId: 8
       },
       {
         name: 'Aeropuerto Internacional de Maiquetía Simón Bolívar',
         smooking: true,
+        ContactInfoId: 9
       },
       {
         name: 'Aeropuerto de París-Charles de Gaulle',
         smooking: true,
+        ContactInfoId: 10
       },
       {
         name: 'Aeropuerto Internacional Mariscal Lamar',
         smooking: true,
+        ContactInfoId: 11
       },
       {
         name: 'Aeropuerto Internacional James M. Cox-Dayton',
         smooking: false,
+        ContactInfoId: 12
       },
       {
         name: 'Aeropuerto Internacional Hamad',
         smooking: false,
+        ContactInfoId:13
       },
       {
         name: 'Aeropuerto de Edimburgo',
         smooking: true,
+        ContactInfoId:14
       },
       {
         name: 'Aeropuerto Internacional de São Paulo-Guarulhos',
         smooking: true,
+        ContactInfoId: 15
       },
       {
         name: 'Aeropuerto de Hannover',
         smooking: false,
+        ContactInfoId: 16
       },
     ]);
     },
--- SRC/seeders/20200711090623-contactInfo.js
+++ SRC/seeders/20200711090623-contactInfo.js
@@ -4,6 +4,7 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('contactInfos',
     [
+      //Airport
       {address:"Palomino Street 30", telephone:"123456",email:"contact@info.com"},
       {address:"Menganito Street 15", telephone:"789123",email:"contact@info.com"},
       {address:"Fulanito Street 28", telephone:"456789",email:"contact@info.com"},
@@ -20,6 +21,7 @@ module.exports = {
       {address:"5th Avenue", telephone:"159753",email:"contact@info.com"},
       {address:"Palomino Street 30", telephone:"123456",email:"contact@info.com"},
       {address:"Menganito Street 15", telephone:"789123",email:"contact@info.com"},
+      //
       {address:"Fulanito Street 28", telephone:"456789",email:"contact@info.com"},
       {address:"Sesame Street", telephone:"147258",email:"contact@info.com"},
       {address:"Yellowstone Park", telephone:"369147",email:"contact@info.com"},
