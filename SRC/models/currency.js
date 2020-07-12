6ed0320562a9392d88fc3a05809bab430cc02ceb
Christian <chsanleo@gmail.com>
2020-07-12 22:46:33 +0200

    Fill the data of current seed, (waiting country)

--- SRC/models/currency.js
+++ SRC/models/currency.js
@@ -1,14 +1,10 @@
 'use strict';
 const {
-  Model
+  Model 
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Currency extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+
     static associate(models) {
       Currency.hasMany(models.Flight);
       Currency.belongsTo(models.Country);
