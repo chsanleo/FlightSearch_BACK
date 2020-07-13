7d420770a4fbde5d24a71d04ef97994376d91bcd
Christian <chsanleo@gmail.com>
2020-07-13 19:47:07 +0200

    Fix migration new field

--- SRC/migrations/20200709174521-create-flight.js
+++ SRC/migrations/20200709174521-create-flight.js
@@ -40,6 +40,10 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: false
       },
+      CompanyId: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
--- SRC/seeders/20200711095818-flight.js
+++ SRC/seeders/20200711095818-flight.js
@@ -6,7 +6,7 @@ module.exports = {
     return queryInterface.bulkInsert('Flights',
       [
         { price: 500.50, code: "A550B", takeOffDate: today.getDate() + 2, landingDate: today.getDate() + 3, CurrencyId: 49, CompanyId: 4, PlaneId: 1, LandingAirportId: 1, TakeOffAirportId: 2 },
-        { price: 250.2, code: "B660D", takeOffDate: today.getDate() + 5, landingDate: today.getDate() + 7, CurrencyId: 49, CompanyIFd: 6, PlaneId: 2, LandingAirportId: 2, TakeOffAirportId: 1 },
+        { price: 250.2, code: "B660D", takeOffDate: today.getDate() + 5, landingDate: today.getDate() + 7, CurrencyId: 49, CompanyId: 6, PlaneId: 2, LandingAirportId: 2, TakeOffAirportId: 1 },
         { price: 1564.0, code: "1550A", takeOffDate: today.getDate() + 4, landingDate: today.getDate() + 4, CurrencyId: 49, CompanyId: 7, PlaneId: 4, LandingAirportId: 4, TakeOffAirportId: 1 },
         { price: 7895.2, code: "YUR89", takeOffDate: today.getDate() + 1, landingDate: today.getDate() + 2, CurrencyId: 49, CompanyId: 4, PlaneId: 1, LandingAirportId: 7, TakeOffAirportId: 3 },
         { price: 1223.0, code: "AXA20", takeOffDate: today.getDate() + 1, landingDate: today.getDate() + 1, CurrencyId: 49, CompanyId: 50, PlaneId: 4, LandingAirportId: 3, TakeOffAirportId: 2 }
