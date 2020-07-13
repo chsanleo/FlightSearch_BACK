5e8085d3eda924d2ff0bd6227b7ab9413acd4dad
Christian <chsanleo@gmail.com>
2020-07-13 19:17:13 +0200

    Create seed for relation fligh and company

--- SRC/seeders/20200711095818-flight.js
+++ SRC/seeders/20200711095818-flight.js
@@ -5,11 +5,11 @@ module.exports = {
     let today = new Date();
     return queryInterface.bulkInsert('Flights',
       [
-        { price: 500.50, code: "A550B", takeOffDate: today.getDate() + 2, landingDate: today.getDate() + 3, CurrencyId:49 },
-        { price: 250.2, code: "B660D", takeOffDate: today.getDate() + 5, landingDate: today.getDate() + 7, CurrencyId:49 },
-        { price: 1564.0, code: "1550A", takeOffDate: today.getDate() + 4, landingDate: today.getDate() + 4, CurrencyId:49 },
-        { price: 7895.2, code: "YUR89", takeOffDate: today.getDate() + 1, landingDate: today.getDate() + 2, CurrencyId:49 },
-        { price: 1223.0, code: "AXA20", takeOffDate: today.getDate() + 1, landingDate: today.getDate() + 1, CurrencyId:49 }
+        { price: 500.50, code: "A550B", takeOffDate: today.getDate() + 2, landingDate: today.getDate() + 3, CurrencyId: 49, CompanyId: 4 },
+        { price: 250.2, code: "B660D", takeOffDate: today.getDate() + 5, landingDate: today.getDate() + 7, CurrencyId: 49, CompanyId: 6 },
+        { price: 1564.0, code: "1550A", takeOffDate: today.getDate() + 4, landingDate: today.getDate() + 4, CurrencyId: 49, CompanyId: 7 },
+        { price: 7895.2, code: "YUR89", takeOffDate: today.getDate() + 1, landingDate: today.getDate() + 2, CurrencyId: 49, CompanyId: 4 },
+        { price: 1223.0, code: "AXA20", takeOffDate: today.getDate() + 1, landingDate: today.getDate() + 1, CurrencyId: 49, CompanyId: 50 }
       ]);
   },
 
