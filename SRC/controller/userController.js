ca131ba1c6895332b25bd97cb750bb2803bcb342
Christian <chsanleo@gmail.com>
2020-07-15 01:03:28 +0200

    Create validations for id in UserController

--- SRC/controller/userController.js
+++ SRC/controller/userController.js
@@ -1,35 +1,38 @@
-const {User} = require('../models');
-const {ContactInfo} = require ('../models');
-const Validations = require ('../utiles/validations');
+const { User } = require('../models');
+const { ContactInfo } = require('../models');
+const Validations = require('../utiles/validations');
 
 const axios = require('axios');
 
 const UserController = {
-    async getAllUsers(req,res) {
+    async getAllUsers(req, res) {
         try {
             const users = await User.findAll();
             res.status(200).send(users);
         } catch (error) {
             console.log(error);
-            res.status(500).send({ message : 'There was a problem'})
+            res.status(500).send({ message: 'There was a problem' })
         }
     },
-    async getOneUser(req,res) {
+    async getOneUser(req, res) {
         try {
             const { id } = req.params;
+            Validations.validaId(id);
+
             const user = await User.findOne({
-                where : {
-                    id: id
-                }
-            })
+                where: { id: id, deletedAt: null }
+            });
+
             res.status(200).send(user)
         } catch (error) {
             console.log(error)
-            res.status(500).send({ message : 'There was a problem'})
+            res.status(500).send({ message: 'There was a problem' })
         }
     },
-    async updateUser(req,res) {
+    async updateUser(req, res) {
         try {
+            const { id } = req.body;
+            Validations.validaId(id);
 
             const contactInfoF = {
                 address: req.body.address,
@@ -50,38 +53,36 @@ const UserController = {
                 questionSecret: req.body.questionSecret,
                 answerSecret: req.body.answerSecret,
                 countryId: req.body.countryId,
-                contactInfoId :contactInfo.id
+                contactInfoId: contactInfo.id
             };
 
             Validations.validaUser(userF);
 
-            const { id } = req.body;
             await User.update(userF, {
                 where: {
                     id: id
                 }
             })
-            res.status(200).send({ message : 'Todo OK'})
+            res.status(200).send({ message: 'Todo OK' })
         } catch (error) {
             console.log(error)
-            res.status(500).send({ message : 'There was a problem'})
+            res.status(500).send({ message: 'There was a problem' })
         }
     },
-    async deleteUser(req,res){
+    async deleteUser(req, res) {
         try {
             const { id } = req.params;
+            Validations.validaId(id);
+
             await User.destroy({
-                where: {
-                    id: id
-                }
+                where: { id: id }
             })
-            res.status(200).send({ message : 'Todo OK'})
+            res.status(200).send({ message: 'Todo OK' })
         } catch (error) {
             console.log(error)
-            res.status(500).send({ message : 'There was a problem'})
+            res.status(500).send({ message: 'There was a problem' })
         }
     }
-
 };
 
 module.exports = UserController;
