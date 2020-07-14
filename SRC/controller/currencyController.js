c01869eaa2d82674b65a1b5a16c2851056bb34b8
Christian <chsanleo@gmail.com>
2020-07-15 00:09:03 +0200

    Apply deleteAt in CurrencyController

--- SRC/controller/currencyController.js
+++ SRC/controller/currencyController.js
@@ -1,6 +1,5 @@
-const {Currency} = require('../models');
+const { Currency } = require('../models');
 
-const axios = require('axios');
 const Validations = require('../utiles/validations');
 
 const CurrencyController = {
@@ -12,7 +11,7 @@ const CurrencyController = {
                 countryId: req.body.countryId
             };
 
-            Validations.validaCurrency(currencyF)
+            Validations.validaCurrency(currencyF);
 
             const currency = await Currency.create(currencyF);
             res.status(201).send(currency);
@@ -22,35 +21,31 @@ const CurrencyController = {
             res.status(500).send({ message: 'There was an error creating the Currency.' });
         }
     },
-    async getCurrencies(req,res) {
+    async getCurrencies(req, res) {
         try {
-            const currencies = await Currency.findAll({
-                limit: 15
-            })
-            res.status(200).send(currencies)
+            const currencies = await Currency.findAll({ where: { deletedAt: null } });
+            res.status(200).send(currencies);
         } catch (error) {
-            console.log(error)
-            res.status(500).send({ message : 'There was a problem trying to get the currencies'})
+            console.log(error);
+            res.status(500).send({ message: 'There was a problem trying to get the currencies' });
         }
     },
-    async getCurrencyById(req,res) {
+    async getCurrencyById(req, res) {
         try {
             const { id } = req.params;
             const currencyId = await Currency.findOne({
-                where : {
-                    id : id
-                }
-            })
-            if (currencyId === null){
-                res.status(400).send({ message : 'There was a problem getting the specified currency.'})
+                where: { deletedAt: null, id: id }
+            });
+            if (currencyId === null) {
+                res.status(400).send({ message: 'There was a problem getting the specified currency.' });
             }
             res.status(200).send(currencyId);
         } catch (error) {
             console.log(error)
-            res.status(500).send({ message : 'There was a problem refreshing the currency.'})
+            res.status(500).send({ message: 'There was a problem refreshing the currency.' });
         }
     },
-    async updateCurrency(req,res) {
+    async updateCurrency(req, res) {
         try {
             const currencyF = {
                 name: req.body.name,
@@ -58,30 +53,25 @@ const CurrencyController = {
                 countryId: req.body.countryId
             };
 
-            Validations.validaFlight(currencyF)
+            Validations.validaFlight(currencyF);
 
+            await Currency.update(currencyF, { where: { id: req.body } });
 
-            await Currency.update(currencyF, {
-                where: { id: req.body }
-            });
             res.status(202).send({ message: 'Successfull Updated.' });
         } catch (error) {
             console.log(error);
             res.status(500).send({ message: 'There was an error.' });
         }
     },
-    async deleteCurrency(req,res) {
-        try {
-            const { id } = req.params
+    async deleteCurrency(req, res) {
+        try { 
             const currency = await Currency.destroy({
-                where : {
-                    id : id
-                }
-            })
-            res.status(200).send({ message : 'Currency deleted succesfully.'})
+                where: {vid: req.params}
+            });
+            res.status(200).send({ message: 'Currency deleted succesfully.' });
         } catch (error) {
             console.log(error)
-            res.status(500).send({ message : 'There was a problem deleting the currency, please, try again.'})
+            res.status(500).send({ message: 'There was a problem deleting the currency, please, try again.' });
         }
     }
 };
