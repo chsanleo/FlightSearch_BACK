24f335716121eef4aa324dda907e2ce2b44558b6
hanschris2 <hanschris96@gmail.com>
2020-07-14 19:58:19 +0200

    Prueba de User

--- SRC/routes/users.js
+++ SRC/routes/users.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const UserController = require('../controller/userController');
 
 router.get('/allusers', UserController.getAllUsers);
-router.get('/user/:id', UserController.getOneUser);
+router.get('/:id',UserController.getOneUser);
 router.put('/modify/:id', UserController.updateUser);
 router.delete('/delete/:id', UserController.deleteUser);
 
