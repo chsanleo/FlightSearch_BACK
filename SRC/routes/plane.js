070efe0dfc23d44b60e57fbb4de1d4b7b2a6ea3f
Jesus1994-7 <jesus1994_cg@hotmail.com>
2020-07-15 21:20:13 +0200

    Testing Plane

--- SRC/routes/plane.js
+++ SRC/routes/plane.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const PlaneController = require ('../controller/planeController')
 
 router.get('/allplanes', PlaneController.getPlanes);
-router.get('/planes/:id', PlaneController.getOnePlane);
+router.get('/:id', PlaneController.getOnePlane);
 router.post('/create', PlaneController.createPlane);
 router.put('/modify/:id', PlaneController.updatePlane);
 router.delete('/delete/:id', PlaneController.deletePlane);
