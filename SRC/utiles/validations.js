a6e28d6e8c19fa82ce11d2def3ea91b48890c029
Christian <chsanleo@gmail.com>
2020-07-28 01:10:22 +0200

    need review email regex

--- SRC/utiles/validations.js
+++ SRC/utiles/validations.js
@@ -26,16 +26,16 @@ const Validations = {
     validaContactInfo(contactInfo) {
 
         let error = EMPTY;
-        let regex = /^(?:[a-z0-9!#$%&amp;'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&amp;'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/;
+        let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
         if (contactInfo.address == EMPTY || contactInfo.address === undefined) {
             error += ' Must provide a address. ';
         }
 
         if (contactInfo.email != EMPTY) {
-            if (!regex.test(contactInfo.email)) {
+           /* if (!regex.test(String(contactInfo.email).toLowerCase())) {
                 error += ' The format email is invalid. ';
-            }
+            }*/
         }
         else { error += ' Must provide a email. '; }
 
